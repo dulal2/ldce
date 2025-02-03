760ebbe0e31b1d10809f007bde52af5c6b2d12ee
dulal <89677740+dulal2@users.noreply.github.com>
2025-02-03 11:20:53 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,13 +3,13 @@ export default function AboutPage() {
     <div className="max-w-3xl mx-auto">
       <h1 className="text-3xl font-bold mb-4">About train manager quiz</h1>
       <p className="mb-4">
-        SER ADRA Quiz is a specialized platform designed to test and expand your knowledge on important topics related
-        to the South Eastern Railway Adra Division. Our quizzes cover a wide range of subjects, including Important Days
-        & Dates, Train Management, and Rajbhasa.
+        ldce gtm Quiz is a specialized platform designed to test and expand your knowledge on important topics related
+        to the operating and commercial department. Our quizzes cover a wide range of subjects, including Important Days
+        & Dates, Train Manager, and Rajbhasa.
       </p>
       <p>
         Whether you're a railway employee looking to reinforce your learning, preparing for examinations, or just
-        someone who enjoys challenging themselves with railway-related knowledge, SER ADRA Quiz has something for
+        someone who enjoys challenging themselves with railway-related knowledge, ldce gtm quiz has something for
         everyone. Start exploring our quizzes today and enhance your understanding of railway operations and policies!
       </p>
     </div>
