4200a47771dffa758b5d6ce05abf72645ffc2f45
dulal <89677740+dulal2@users.noreply.github.com>
2025-02-03 11:24:07 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -3,7 +3,7 @@ export default function ContactPage() {
     <div className="max-w-3xl mx-auto">
       <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
       <p className="mb-4">
-        We'd love to hear from you! If you have any questions, suggestions, or feedback about SER ADRA Quiz, please
+        We'd love to hear from you! If you have any questions, suggestions, or feedback about ldce gtm, please
         don't hesitate to get in touch with us.
       </p>
       <div className="mb-4">
@@ -12,8 +12,8 @@ export default function ContactPage() {
       </div>
       <div>
         <h2 className="text-xl font-semibold mb-2">Address</h2>
-        <p>SER ADRA PRIVATE GROUP</p>
-        <p>ADRA, 723121</p>
+        <p>ldce gtm</p>
+        <p>west bengal, 723121</p>
         <p>INDIA</p>
       </div>
     </div>
