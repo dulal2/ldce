dd7dd25cbcce057f6f67e41fbc758854369f3978
dulal <89677740+dulal2@users.noreply.github.com>
2025-02-05 17:35:41 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,14 +3,11 @@ export default function AboutPage() {
     <div className="max-w-3xl mx-auto">
       <h1 className="text-3xl font-bold mb-4">About train manager quiz</h1>
       <p className="mb-4">
-        ldce gtm Quiz is a specialized platform designed to test and expand your knowledge on important topics related
-        to the operating and commercial department. Our quizzes cover a wide range of subjects, including Important Days
-        & Dates, Train Manager, and Rajbhasa.
+        ldce gtm Quiz is a specialized platform designed to test and expand your knowledge on important topics related Important Days
+        & Dates, Train Manager, and Rajbhasa. Train manager tropic daily update english or hindi language
       </p>
       <p>
-        Whether you're a railway employee looking to reinforce your learning, preparing for examinations, or just
-        someone who enjoys challenging themselves with railway-related knowledge, ldce gtm quiz has something for
-        everyone. Start exploring our quizzes today and enhance your understanding of railway operations and policies!
+        kEEP EYES FOR DALILY UPDATES 
       </p>
     </div>
   )
