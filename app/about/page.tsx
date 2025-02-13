ca857b80a76b31c69aab28ae5824f6ae97ae5914
dulal <89677740+dulal2@users.noreply.github.com>
2025-02-13 21:04:12 +0530

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function AboutPage() {
   return (
     <div className="max-w-3xl mx-auto">
-      <h1 className="text-3xl font-bold mb-4">About QuizMaster</h1>
+      <h1 className="text-3xl font-bold mb-4">About LdceGtm</h1>
       <p className="mb-4">
         ldce gtm Quiz is a specialized platform designed to test and expand your knowledge on important topics related Important Days
         & Dates, Train Manager, and Rajbhasa. Train manager tropic daily update english or hindi language
