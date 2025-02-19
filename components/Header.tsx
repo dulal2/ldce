7b098a910754b696664579905614d70954d7608c
dulal <89677740+dulal2@users.noreply.github.com>
2025-02-19 12:19:36 +0530

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
               <div className="relative w-8 h-8 rounded-full overflow-hidden border-2 border-gray-50">
                 <Image
                   src="/logo.png"
-                  alt="Train Manager Quiz Logo"
+                  alt="ldceGtm Logo"
                   fill
                   className="object-cover"
                   priority
