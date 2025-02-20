641e6ff0d2bad303a3bc3412dc472ae4ec4543a0
dulal <89677740+dulal2@users.noreply.github.com>
2025-02-20 11:31:03 +0530

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,20 +1,20 @@
 export default function ContactPage() {
   return (
     <div className="max-w-3xl mx-auto">
-      <h1 className="text-3xl text-gray-50 font-bold mb-4">Contact Us</h1>
-      <p className="mb-4  text-gray-50">
+      <h1 className="text-3xl font-bold mb-4">Contact Us</h1>
+      <p className="mb-4">
         We'd love to hear from you! If you have any questions, suggestions, or feedback about ldce gtm, please
         don't hesitate to get in touch with us.
       </p>
-      <div className="mb-4  text-gray-50">
-        <h2 className="text-xl  text-gray-50 font-semibold mb-2">Email</h2>
+      <div className="mb-4">
+        <h2 className="text-xl font-semibold mb-2">Email</h2>
         <p>ldcetgm@zohomail.in</p>
       </div>
       <div>
-        <h2 className="text-xl  text-gray-50 font-semibold mb-2">Address</h2>
-        <p className="mb-4  text-gray-50">ldce gtm</p>
-        <p className="mb-4  text-gray-50">W.B,723121</p>
-        <p className="mb-4  text-gray-50">INDIA</p>
+        <h2 className="text-xl font-semibold mb-2">Address</h2>
+        <p>ldce gtm</p>
+        <p>west bengal, 723121</p>
+        <p>INDIA</p>
       </div>
     </div>
   )
